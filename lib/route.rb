f8b8af4556dad6acf76c3d413a6cc19c50a6e7f0
Sahil_Kulkarni <sahil.rordev@gmail.com>
2015-08-06 13:10:03 +0530

    Added code to calculate distances between source and destination

--- lib/route.rb
+++ lib/route.rb
@@ -5,8 +5,7 @@ class Route
   end
 
   def calc_distance_btw_towns(*args)
-    towns = Town.new @routes
-    src_dest,distance = towns.create_towns_distance
+    src_dest,distance = source_destination_distance
     distance = Route.distance_calculator(src_dest,distance,args,distance=[])
   end
 
@@ -21,8 +20,95 @@ class Route
     end
     return distance.reduce(:+)
     rescue
-      "NO ROUTE"
+      "NO SUCH ROUTE"
     end
+  end
+
+  def calc_max_trips_three_stops(src,dest,max_stops)
+    start_with_c = []
+    end_with_c = []
+    src_dest,dist = source_destination_distance
+    src_dest.each do |split_by_c|
+      start_with_c << split_by_c if split_by_c[0] == src
+      end_with_c << split_by_c if split_by_c[1] == dest
+    end
+    max_trips = check_routes(start_with_c,end_with_c,src_dest)
+    return max_trips.length if max_trips.each{|max_stops| max_stops.count == 3 }
+  end
+
+  def check_routes(start_with_c,end_with_c,src_dest)
+    i = 0
+    route = []
+    start_with_c = start_with_c.delete_if{|val| end_with_c.include?(val.reverse)}
+    source = start_with_c[i][i+1]
+    end_with_c.each do |end_of_route|
+      if src_dest.include?(start_with_c[i]) && src_dest.include?(end_of_route)
+        destination = end_of_route[i][i]
+        connecting_station = source+destination
+        if src_dest.include?(connecting_station)
+          route << [start_with_c[i],connecting_station,end_of_route]
+        elsif src_dest.include?(connecting_station.reverse)
+          route << [start_with_c[i],connecting_station,end_of_route]
+        end
+      end
+    end
+    return route
+  end
+
+  def shortest_distance_src_dest(src,dest)
+    shortest_distance = []
+    src_dest,dist = source_destination_distance
+    source = src
+    destination = dest
+    all_src_dest_routes = find_all_src_dest_routes(source,destination,src_dest,eliminate_elements=[])
+    shortest_route = shortest_route(all_src_dest_routes,src,dest,dist,shortest_distance,src_dest)
+  end
+
+  def find_all_src_dest_routes(source,destination,src_dest,eliminate_elements)
+    eliminate_elements << src_dest
+    eliminate_elements.flatten.delete_if{|route| route[0] != source && route[1] != destination}
+  end
+
+  def shortest_route(all_src_dest_routes,src,dest,dist,shortest_distance,src_dest)
+    routes = all_src_dest_routes
+    routes.each do |current_town|
+      if current_town[0] == src
+        destination = current_town[1]
+        dest_town = routes.select{|x| x.start_with?(destination) && x.end_with?(dest)}
+        begin
+          calc_distance = dist[src_dest.index(current_town)].to_i + dist[src_dest.index(dest_town.first)].to_i
+        rescue
+          calc_distance = "INVALID"
+        end
+          shortest_distance << calc_distance
+      end
+    end
+    return shortest_distance.delete_if{|x| x.kind_of? String}.min
+  end
+
+  def shortest_dist_same_src_dest(src,dest)
+    same_source = []
+    same_dest = []
+    src_dest,dist = source_destination_distance
+    src_dest.each do |splitter|
+      same_source << splitter if splitter[0] == src
+      same_dest << splitter if splitter[1] == dest
+    end
+    max_trips = check_routes(same_source,same_dest,src_dest)
+    max_trips = index_values_of_sources(src_dest,dist,max_trips)
+  end
+
+  def index_values_of_sources(src_dest,dist,sources)
+    shortest_distance = []
+    sources.flatten.each do |x|
+      shortest_distance << dist[src_dest.index(x)].to_i
+    end
+    return shortest_distance.reduce(:+)
+  end
+
+  def source_destination_distance
+    towns = Town.new @routes
+    src_dest,dist = towns.create_towns_distance
   end
 
 end
--- train_routes.rb
+++ train_routes.rb
@@ -3,10 +3,11 @@ require './lib/town.rb'
 require 'pry'
 route = Route.new("AB5", "BC4", "CD8", "DC8", "DE6", "AD5", "CE2", "EB3", "AE7")
 
-puts route.calc_distance_btw_towns("A","B","C")
-puts route.calc_distance_btw_towns("A","D")
-puts route.calc_distance_btw_towns("A","D","C")
-puts route.calc_distance_btw_towns("A","E","B","C","D")
-puts route.calc_distance_btw_towns("A","E","D")
-puts route.calc_max_trips_three_stops("C","C",3)
-puts route.shortest_distance_src_dest("A","C")
+puts "1) The distance of the route A-B-C : #{route.calc_distance_btw_towns("A","B","C")}"
+puts "2) The distance of the route A-D : #{route.calc_distance_btw_towns("A","D")}"
+puts "3) The distance of the route A-D-C : #{route.calc_distance_btw_towns("A","D","C")}"
+puts "4) The distance of the route A-E-B-C-D : #{route.calc_distance_btw_towns("A","E","B","C","D")}"
+puts "5) The distance of the route A-E-D : #{route.calc_distance_btw_towns("A","E","D")}"
+puts "6) The number of trips starting at C and ending at C with a maximum of 3 stops : #{route.calc_max_trips_three_stops("C","C",3)}"
+puts "8) The length of the shortest route from A to C : #{route.shortest_distance_src_dest("A","C")}"
+puts "9) The length of the shortest route from B to B : #{route.shortest_dist_same_src_dest("B","B")}"
